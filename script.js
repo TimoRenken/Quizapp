93740fe4d01dd6bdaf3436f2c74584a9f501d3c2
TimoRenken <timorenken@outlook.de>
2024-02-10 10:34:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let questions = [
+    {
+        "question": "Wie heißt die Waffe der Jedi-Ritter?",
+        "answer_1": "Blaster",
+        "answer_2": "Zwille",
+        "answer_3": "Pistole",
+        "answer_4": "Lichtschwert",
+        "right_answer": 4
+    },
+    {
+        "question": "Wer ist der Vater von Luke Skywalker?",
+        "answer_1": "Sheev Palpatine",
+        "answer_2": "Darth Vader",
+        "answer_3": "Darth Maul",
+        "answer_4": "Anakin Skywalker",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie heißt das Raumschiff von Han Solo?",
+        "answer_1": "Razor Crest",
+        "answer_2": "Supremacy",
+        "answer_3": "Millennium Falke",
+        "answer_4": "Executor",
+        "right_answer": 3
+    },
+    {
+        "question": "Mit welchen Worten verabschieden sich Jedi-Ritter?",
+        "answer_1": "Bis bald!",
+        "answer_2": "Und Tschüss!",
+        "answer_3": "Bye Bye",
+        "answer_4": "Möge die Macht mit dir sein",
+        "right_answer": 4
+    },
+    {
+        "question": "Was für ein Wesen ist Chewbacca?",
+        "answer_1": "Wookie",
+        "answer_2": "Tusken-Räuber",
+        "answer_3": "Twi'lek",
+        "answer_4": "Mensch",
+        "right_answer": 1
+    },
+    {
+        "question": "Leia ist die Schwester von?",
+        "answer_1": "Han Solo",
+        "answer_2": "Luke Skywalker",
+        "answer_3": "Obi-Wan Kenobi",
+        "answer_4": "Yoda",
+        "right_answer": 2
+    },
+    {
+        "question": "Welcher Sith kämpft mit einem Doppel-Laserschwert?",
+        "answer_1": "Darth Vader",
+        "answer_2": "Darth Sidious",
+        "answer_3": "Darth Maul",
+        "answer_4": "Darth Caedus",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt der Freund von C-3PO ?",
+        "answer_1": "BB-8",
+        "answer_2": "R2-D2",
+        "answer_3": "L3-37",
+        "answer_4": "IG-11",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche Farbe hat Lukes Lichtschwert?",
+        "answer_1": "Blau",
+        "answer_2": "Lila",
+        "answer_3": "Rot",
+        "answer_4": "Grün",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie heißt der Lehrer von Luke Skywalker?",
+        "answer_1": "Obi-Wan Kenobi",
+        "answer_2": "Yoda",
+        "answer_3": "Qui-Gon Jinn",
+        "answer_4": "Boba Fett",
+        "right_answer": 2
+    }
+];
+
+function init(){
+    document.getElementById('showArrayLength').innerHTML = questions.length;
+}
